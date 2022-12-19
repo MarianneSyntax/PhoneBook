f5ea221e1b0bb2027cf8573ccee715536c0a726b
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-19 19:39:12 +0000

    5.3 Daten vom ViewModel an HomeFragment weiterreichen, observen und bei Veränderung den noch nicht existenten Adapter aktualisieren.
    (data binding im gradle vergessen)

---
+++ app/src/main/java/com/example/phonebook/ui/HomeFragment.kt
@@ -0,0 +1,46 @@
+package com.example.phonebook.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.example.phonebook.R
+import com.example.phonebook.data.model.Contact
+import com.example.phonebook.databinding.FragmentHomeBinding
+import com.example.phonebook.ui.adapter.ContactAdapter
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //val contactAdapter = ContactAdapter()
+        //binding.recyclerView.adapter = contactAdapter
+
+        // contacts variable aus dem viewModel observen, bei Veränderungen contacts neu in den Adapter schreiben
+        viewModel.contacts.observe(viewLifecycleOwner) {
+            //contactAdapter.submitList(it)
+        }
+
+        // Button fügt einen Listeneintrag hinzu
+        binding.btnAdd.setOnClickListener {
+           //todo
+        }
+    }
+}
--- app/src/main/java/com/example/phonebook/ui/main/HomeFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.example.phonebook.ui.main
-
-import androidx.fragment.app.Fragment
-
-class HomeFragment: Fragment() {
-}
