af0ed131984b5062bb9518b45121d049af722b44
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-19 20:08:22 +0000

    6.2 insert() im Repository aufrufen, einzufügender Kontakt wird beim Aufruf übergeben

--- app/src/main/java/com/example/phonebook/data/Repository.kt
+++ app/src/main/java/com/example/phonebook/data/Repository.kt
@@ -2,12 +2,18 @@ package com.example.phonebook.data
 
 import com.example.phonebook.data.local.ContactDatabase
 import com.example.phonebook.data.local.getDatabase
+import com.example.phonebook.data.model.Contact
 
 // Repository mit Datenbank im Konstruktor
 class Repository(private val database: ContactDatabase) {
 
     // Zugriff auf getAll() über die Datenbank aus dem Konstrutkor, die eine abstract DAO hat
     val contacts = database.contactDatabaseDao.getAll()
+
+    // übergebenen Kontakt einfügen
+    suspend fun insertContact(contact: Contact) {
+        database.contactDatabaseDao.insert(contact)
+    }
 
 
 }
