31bae78d02f96ef300a633d5f7466fa93ccb7115
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-19 18:24:08 +0000

    1. Schritt: Layouting, nav_graph, gradles vorbereiten

--- app/src/main/java/com/example/phonebook/MainActivity.kt
+++ app/src/main/java/com/example/phonebook/MainActivity.kt
@@ -2,17 +2,12 @@ package com.example.phonebook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.phonebook.ui.main.MainFragment
+import com.example.phonebook.ui.main.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+
     }
 }
---
+++ app/src/main/java/com/example/phonebook/ui/main/HomeFragment.kt
@@ -0,0 +1,6 @@
+package com.example.phonebook.ui.main
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment: Fragment() {
+}
