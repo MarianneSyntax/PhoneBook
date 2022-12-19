98b62f521265da6b648b19ab19167d70578aa39e
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-19 18:52:56 +0000

    Lokale Datenbank: 4. Schritt: Database vervollständigen

--- app/src/main/java/com/example/phonebook/data/local/ContactDatabase.kt
+++ app/src/main/java/com/example/phonebook/data/local/ContactDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.phonebook.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.phonebook.data.model.Contact
 
@@ -13,8 +15,25 @@ class ContactDatabase {
         abstract val contactDatabaseDao: ContactDatabaseDao
     }
 
-    //todo: diese ContactDatabase in die instanz variable schreiben, sodass unsere app nur diese ein ContactDatabase hat
+    // diese ContactDatabase in die instanz variable schreiben, sodass unsere app nur diese ein ContactDatabase hat
 
-    //todo getDatabase() Funktion schreiben, die eine ContactDatabase baut, falls noch keine vorhanden ist
+    private lateinit var INSTANCE: ContactDatabase
+
+    // getDatabase() Funktion, die eine ContactDatabase baut, falls noch keine vorhanden ist
+    fun getDatabase(context: Context): ContactDatabase {
+        synchronized(ContactDatabase::class.java) {
+            if (!::INSTANCE.isInitialized) {
+                INSTANCE = Room.databaseBuilder(
+                    context.applicationContext,
+                    ContactDatabase::class.java,
+                    "contact_database_4"
+                )
+                    .build()
+            }
+        }
+        return INSTANCE
+    }
+
+
 
 }
