4c6fe0ac7d3f550363f4ac3f52e774f5de7bdaa1
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-19 20:10:14 +0000

    6.3 insertContact() aus einer Coroutine heraus im ViewModel aufrufen mit try catch

--- app/src/main/java/com/example/phonebook/ui/MainViewModel.kt
+++ app/src/main/java/com/example/phonebook/ui/MainViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.phonebook.ui
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.phonebook.data.Repository
 import com.example.phonebook.data.local.getDatabase
+import com.example.phonebook.data.model.Contact
+import kotlinx.coroutines.launch
 
 // neue Syntax im ViewModel Kopf! Android application
 class MainViewModel(application: Application) : AndroidViewModel(application) {
@@ -16,4 +20,14 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     // speichert das Ergebnis vom repo aufruf von getAll() aus dem DAO
     val contacts = repo.contacts
+
+    fun insertContact(contact: Contact) {
+        viewModelScope.launch {
+            try {
+                repo.insertContact(contact)
+            } catch (e: Exception) {
+                Log.e("MainViewModel", "failed inserting contact: $e")
+            }
+        }
+    }
 }
